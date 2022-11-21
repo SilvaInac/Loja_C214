aed2f6d0fab74741dcc1fc380775f028a23c4fe2
Camila Silva <camila.silva@watchguard.com>
2022-11-21 12:16:09 -0300

    Adicionando funções e rotas

--- loja/src/app/app-routing.module.ts
+++ loja/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: AppComponent },
+  { path: 'home/carrinho', component: CarrinhoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- loja/src/app/app.module.ts
+++ loja/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
