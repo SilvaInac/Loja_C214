73f7ce603e649a7faff7f30bba726394d43c30d3
Camila Silva <camila.silva@watchguard.com>
2022-11-28 00:26:14 -0300

    Deixando o front mais dinâmico

--- loja/src/app/home/home.component.ts
+++ loja/src/app/home/home.component.ts
@@ -7,19 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  produtos = [
+    {id: 1, nome: 'Adidas',preco: 239.50},
+    {id: 2, nome: 'Nike',preco: 439.50},
+    {id: 3, nome: 'Puma',preco: 189.50},
+    {id: 4, nome: 'Keds', preco: 255.60}
+  ];
+  palavra: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  adicionarProduto() {
+  adicionarProduto(idProduto: number) {
+    document.getElementById('btn-done-'+idProduto)?.removeAttribute('hidden');
+    document.getElementById('btn-add-'+idProduto)?.setAttribute('hidden', 'true');
 
   }
 
   verCarrinho() {
   }
 
-  buscar(){
+  buscar(palavra: string){
+    if (palavra) {
+      palavra = palavra.toUpperCase();
 
+      this.produtos = this.produtos.filter(a =>
+        a.nome.toUpperCase().indexOf(palavra) >= 0
+      );
+    } else {
+      return;
+    }
   }
 }
